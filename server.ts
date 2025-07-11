42e0e69dc3ca7b6cb1c610805f5739ec623cbf1c
DutchBrat <me@dutchbrat.com>
2025-07-11 18:26:41 +0000

    didn't make it in the previous push

--- server.ts
+++ server.ts
@@ -8,7 +8,6 @@ const port = parseInt(process.env.PORT || '3000', 10);
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
-const articleRoutes = require('./routes/articles');
 
 app.prepare().then(() => {
     const server = express();
